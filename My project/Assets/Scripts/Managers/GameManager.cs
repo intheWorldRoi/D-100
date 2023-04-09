d3af63cee3b6f6e355975460f6b385495e640a87
JuHa <underthesun7213@gmail.com>
2023-04-09 13:42:59 +0900

    test

--- My project/Assets/Scripts/Managers/GameManager.cs
+++ My project/Assets/Scripts/Managers/GameManager.cs
@@ -12,7 +12,7 @@ public class GameManager : MonoBehaviour
     public static int monthday = 22; //일
 
     public static string TODAY; // 오늘은 무슨 요일인
-    public static int week =1; // 몇번째 주 인가
+    public static int week =2; // 몇번째 주 인가
 
     public static int money = 100;
 
