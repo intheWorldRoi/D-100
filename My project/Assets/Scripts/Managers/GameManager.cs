54deaa59e208014236c4f95da527d09ca7dc99c5
jimmy <6796kim@naver.com>
2023-04-09 13:49:57 +0900

    test1

--- My project/Assets/Scripts/Managers/GameManager.cs
+++ My project/Assets/Scripts/Managers/GameManager.cs
@@ -7,12 +7,12 @@ using TMPro;
 public class GameManager : MonoBehaviour
 {
 
-    public static int Day = 12; //며칠이나 지났나
+    public static int Day = 11213; //며칠이나 지났나
     public static int month = 11; //월
     public static int monthday = 22; //일
 
     public static string TODAY; // 오늘은 무슨 요일인
-    public static int week =2; // 몇번째 주 인가
+    public static int week =1; // 몇번째 주 인가
 
     public static int money = 100;
 
