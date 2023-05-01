5941e8261e4c540dd297e020de8cad00af3e4543
JuHa <underthesun7213@gmail.com>
2023-05-01 23:11:03 +0900

    게임엔딩 크레딧 띄우기 전 효과

--- My project/Assets/Scripts/Animations/EndingCredit.cs
+++ My project/Assets/Scripts/Animations/EndingCredit.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.Video;
 
 public class EndingCredit : MonoBehaviour
 {
     float alpha;
     float y;
+    float[] alphaT;
+    VideoPlayer player;
+
+    GameObject texts;
     // Start is called before the first frame update
 
     private void Awake()
     {
-
+        alphaT = new float[8] { 0, 0, 0, 0, 0, 0, 0, 0 };
         alpha = 0.1f;
         y = -60f;
         transform.GetComponent<Image>().color = new Color(0, 0, 0, 0);
@@ -23,7 +29,6 @@ public class EndingCredit : MonoBehaviour
     {
         StartCoroutine(Fadein());
         Invoke("replay", 5f);
-        
     }
 
     // Update is called once per frame
@@ -35,6 +40,32 @@ public class EndingCredit : MonoBehaviour
             transform.GetComponent<Image>().color = new Color(0, 0, 0, alpha);
             alpha += 0.1f;
         }
+    }
+    
+    IEnumerator fadein(GameObject o)
+    {
+        for(int i = 0; i < alphaT.Length; i++)
+        {
+            while (alphaT[i] <= 1f)
+            {
+                yield return new WaitForSeconds(0.1f);
+                o.transform.GetChild(i).GetComponent<TextMeshProUGUI>().color = new Color(255, 255, 255, alphaT[i]);
+                alphaT[i] += 0.1f;
+            }
+            yield return new WaitForSeconds(1f);
+        }
+        
+       
+    }
+    void txtEff()
+    {
+        StartCoroutine(fadein(texts));
+    }
+
+    void playFog()
+    {
+        player.Play();
+
     }
     private void replay()
     {
--- My project/Assets/Scripts/DialogueS/TypeEffect.cs
+++ My project/Assets/Scripts/DialogueS/TypeEffect.cs
@@ -58,7 +58,7 @@ public class TypeEffect : MonoBehaviour
     {
        
         
-        s.StopSound();
+        //s.StopSound();
 
     }
 }
