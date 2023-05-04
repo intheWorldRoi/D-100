5fe6ddc6e48840a49cc6e95d723cef70dcfd0942
jimmy <6796kim@naver.com>
2023-05-04 12:10:22 +0900

    개발자모드

---
+++ My project/Assets/Scripts/CheatButton.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheatButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void healthyPlus()
+    {
+        StatusManager.healthy += 10;
+    }
+
+    public void healthyMinus()
+    {
+        StatusManager.healthy -= 10;
+    }
+
+    public void moneyPlus()
+    {
+        GameManager.money += 10;
+    }
+
+    public void moneyMinus()
+    {
+        GameManager.money -= 10;
+    }
+
+    public void EngPlus()
+    {
+        StatusManager.Engknowledge += 10;
+    }
+
+    public void EngMinus()
+    {
+        StatusManager.Engknowledge -= 10;
+    }
+
+    public void innerPlus()
+    {
+        StatusManager.innerpeace += 10;
+    }
+
+    public void innerMinus()
+    {
+        StatusManager.innerpeace -= 10;
+    }
+}
